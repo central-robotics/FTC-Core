142b52c440fc13f83a1d210ed3dfe33608750b23
Zack Murry <zackk.murryy@gmail.com>
2022-09-11 14:18:18 -0500

    Remove submodule

---
+++ sample/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestOpMode.java
@@ -0,0 +1,14 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+@Autonomous
+public class TestOpMode extends LinearOpMode {
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+    }
+
+}
