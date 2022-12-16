7cbb21b9a4bfd797523b06b5a6f4902c8f1843e8
danielhuinda <huindam@gmail.com>
2022-12-16 14:40:08 -0600

    change theta tolerance

--- modules/core/src/main/java/com/chsrobotics/ftccore/engine/navigation/NavigationEngine.java
+++ modules/core/src/main/java/com/chsrobotics/ftccore/engine/navigation/NavigationEngine.java
@@ -51,7 +51,11 @@ public class NavigationEngine {
         if (thetaError < 0 && (thetaError > -Math.PI))
             isCounterClockwise = false;
 
+<<<<<<< HEAD
         return (error < 40 && Math.abs(thetaError) < 0.09);
+=======
+        return (error < 40 && Math.abs(thetaError) < 0.070);
+>>>>>>> 35593e3 (change theta tolerance)
     }
 
     public void navigateInALinearFashion(Position destination)
