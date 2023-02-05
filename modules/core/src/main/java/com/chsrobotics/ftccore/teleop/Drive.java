4576faa31b3a7d33159399ab60dfb235cbb8f689
danielhuinda <huindam@gmail.com>
2023-02-05 13:44:18 -0600

    use PID to correct heading in drive

--- modules/core/src/main/java/com/chsrobotics/ftccore/teleop/Drive.java
+++ modules/core/src/main/java/com/chsrobotics/ftccore/teleop/Drive.java
@@ -3,8 +3,11 @@ package com.chsrobotics.ftccore.teleop;
 import android.icu.number.Scale;
 
 import com.chsrobotics.ftccore.actions.Action;
+import com.chsrobotics.ftccore.engine.navigation.control.PID;
+import com.chsrobotics.ftccore.engine.navigation.control.PIDParams;
 import com.chsrobotics.ftccore.hardware.HardwareManager;
 import com.qualcomm.robotcore.hardware.Gamepad;
+import com.qualcomm.robotcore.hardware.PIDCoefficients;
 
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
@@ -19,12 +22,16 @@ public class Drive {
     public static double linearMax = 1;
     public static double rotationMax = 1;
     public boolean overrideDriveControl = false;
+    private boolean isCounterClockwise = false;
+    private PIDCoefficients correctPIDCoeffs;
     private long prevTime = System.currentTimeMillis();
     private final UserDriveLoop loop;
     private final Builder.ScaleMode mode;
+    private double currentAngle;
 
-    public Drive (HardwareManager manager, UserDriveLoop loop, Builder.ScaleMode mode)
+    public Drive (HardwareManager manager, UserDriveLoop loop, Builder.ScaleMode mode, PIDCoefficients correctPID)
     {
+        this.correctPIDCoeffs = correctPID;
         this.mode = mode;
         this.manager = manager;
         this.loop = loop;
@@ -33,8 +40,15 @@ public class Drive {
 
     private void driveLoop()
     {
+        PID correctPID = null;
+        if (correctPIDCoeffs != null) {
+            correctPID = new PID(new PIDParams(correctPIDCoeffs.p, correctPIDCoeffs.i, correctPIDCoeffs.d));
+        }
+
         while (!manager.opMode.isStopRequested())
         {
+            boolean correct = correctPID != null;
+
             loop.loop();
 
             prevTime = System.currentTimeMillis();
@@ -53,8 +67,6 @@ public class Drive {
 
             joystick_y = -gamepad1.left_stick_y;
             joystick_x = gamepad1.left_stick_x == 0 ? 0.001 : gamepad1.left_stick_x;
-
-            rot_power = (manager.thetaReversed ? -1 : 1) * gamepad1.right_stick_x;
 
             double rawPower = Math.sqrt(Math.pow(joystick_x, 2) + Math.pow(joystick_y, 2));
 
@@ -76,6 +88,38 @@ public class Drive {
 
             theta *= manager.thetaReversed ? -1 : 1;
 
+            isCounterClockwise = false;
+
+            rot_power = 0;
+
+            if (Math.abs(gamepad1.right_stick_x) > 0.001) {
+                rot_power = (manager.thetaReversed ? -1 : 1) * gamepad1.right_stick_x;
+            } else if (correct)
+            {
+
+                double thetaError = currentAngle - theta;
+
+                if (Math.abs(currentAngle - (theta - (2 * Math.PI))) < Math.abs(thetaError))
+                {
+                    thetaError = currentAngle - (theta - (2 * Math.PI));
+                    isCounterClockwise = true;
+                }
+
+                if (Math.abs(currentAngle - (theta + (2 * Math.PI))) < thetaError)
+                {
+                    thetaError = currentAngle - (theta + (2 * Math.PI));
+                    isCounterClockwise = true;
+                }
+
+                if (thetaError > 0 && (thetaError < Math.PI))
+                    isCounterClockwise = true;
+
+                if (thetaError < 0 && (thetaError > -Math.PI))
+                    isCounterClockwise = false;
+
+                rot_power = correctPID.getOutput(Math.abs(thetaError), 0);
+            }
+
             orientation = (Math.atan2(joystick_y, joystick_x) - Math.PI / 4) - theta;
 
             negative_power = (joystick_power * Math.sin(orientation));
@@ -83,20 +127,29 @@ public class Drive {
                     negative_power;
 
             if (!overrideDriveControl)
-                move(positive_power, negative_power, rot_power);
+                move(positive_power, negative_power, ((isCounterClockwise ? 1 : -1) * rot_power), correct);
         }
     }
 
-    public void move(double posinput, double neginput, double rotinput)
+    public void move(double posinput, double neginput, double rotinput, boolean usePID)
     {
         manager.opMode.telemetry.addData("lf", -posinput - rotinput);
         manager.opMode.telemetry.addData("rf", neginput - rotinput);
         manager.opMode.telemetry.addData("lb", -neginput - rotinput);
         manager.opMode.telemetry.addData("rb", posinput - rotinput);
-        manager.getLeftFrontMotor().setVelocity((manager.linearMax * -posinput) - (manager.rotMax * rotinput));
-        manager.getRightFrontMotor().setVelocity((manager.linearMax * neginput) - (manager.rotMax * rotinput));
-        manager.getLeftBackMotor().setVelocity((manager.linearMax * -neginput) - (manager.rotMax * rotinput));
-        manager.getRightBackMotor().setVelocity((manager.linearMax * posinput) - (manager.rotMax * rotinput));
+
+        if (!usePID) {
+            manager.getLeftFrontMotor().setVelocity((manager.linearMax * -posinput) - (manager.rotMax * rotinput));
+            manager.getRightFrontMotor().setVelocity((manager.linearMax * neginput) - (manager.rotMax * rotinput));
+            manager.getLeftBackMotor().setVelocity((manager.linearMax * -neginput) - (manager.rotMax * rotinput));
+            manager.getRightBackMotor().setVelocity((manager.linearMax * posinput) - (manager.rotMax * rotinput));
+        } else
+        {
+            manager.getLeftFrontMotor().setVelocity((manager.linearMax * -posinput) + ((isCounterClockwise ? 1 : -1) * rotinput));
+            manager.getRightFrontMotor().setVelocity((manager.linearMax * neginput) + ((isCounterClockwise ? 1 : -1) * rotinput));
+            manager.getLeftBackMotor().setVelocity((manager.linearMax * -neginput) + ((isCounterClockwise ? 1 : -1) * rotinput));
+            manager.getRightBackMotor().setVelocity((manager.linearMax * posinput) + ((isCounterClockwise ? 1 : -1) * rotinput));
+        }
     }
 
     public void runDriveLoop() {
@@ -108,10 +161,10 @@ public class Drive {
         private final HardwareManager manager;
         private UserDriveLoop loop;
         private ScaleMode mode = ScaleMode.LINEAR;
+        private PIDCoefficients correctPID = null;
 
         public Builder(HardwareManager manager)
         {
-
             this.manager = manager;
         }
 
@@ -127,6 +180,12 @@ public class Drive {
             return this;
         }
 
+        public Builder enableTurnCorrection(PIDCoefficients correctPID)
+        {
+            this.correctPID = correctPID;
+            return this;
+        }
+
         public enum ScaleMode
         {
             LINEAR,
@@ -136,7 +195,7 @@ public class Drive {
 
         public Drive build()
         {
-            return new Drive(manager, loop, mode);
+            return new Drive(manager, loop, mode, correctPID);
         }
     }
 }
